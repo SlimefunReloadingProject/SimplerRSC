9f568831004b0e47cff748bc8a1e93058c657d2d
FengXiang2233 <fjx13125@163.com>
2024-03-03 20:46:18 +0800

    修改提示弹窗

--- core/GUI/AddonsManage/OpenAddons.py
+++ core/GUI/AddonsManage/OpenAddons.py
@@ -1,22 +1,28 @@
 import os
 import yaml
-import tkinter.messagebox
 import customtkinter as ctk
 from CTkToolTip import *
 
-import core.main
-
-# 阻塞主线程弹窗
-class info_window:
-    def info(title:str,text:str):
-        tkinter.messagebox.showinfo(title=title,message=text)
+# 弹窗
+class info_window(ctk.CTkToplevel):
+    def __init__(self,master,title:str,text:str):
+        super().__init__()
+        master.wm_attributes("-topmost",False)
+        self.wm_attributes("-topmost",True)
+        self.resizable(0,0)
+        self.title(title)
+        ctk.CTkLabel(self,text=text).grid(row=0,column=0,padx=50,pady=50)
+        ctk.CTkButton(self,text="确定").grid(row=1,column=0)
 
 # 创建附属
 class CreateAddons(ctk.CTkToplevel):
-    def __init__(self,master):
+    def __init__(self,master,top_win):
         super().__init__(master)
         self.resizable(0,0)
+        self.wm_attributes("-topmost", True)
         self.title("SimplerRSC - 创建新的附属")
+        # 关闭上个窗口
+        top_win.destroy()
         # 附属id
         ctk.CTkLabel(self,text="附属id[必填]").grid(row=0,column=0,padx=10,pady=(10,0),sticky="w")
         self.addon_id=ctk.CTkEntry(self,width=160)
@@ -38,7 +44,7 @@ class CreateAddons(ctk.CTkToplevel):
         self.addon_dir=ctk.CTkEntry(self)
         self.addon_dir.grid(row=5,column=0,padx=8,columnspan=2,sticky="ew")
         #GitHub位置
-        self.githubLabel=ctk.CTkLabel(self,text="GitHub存储库位置[可选]").grid(row=6,column=0,padx=10,pady=(10,0),sticky="w")
+        ctk.CTkLabel(self,text="GitHub存储库位置[可选]").grid(row=6,column=0,padx=10,pady=(10,0),sticky="w")
         self.addon_repo=ctk.CTkEntry(self)
         self.addon_repo.grid(row=7,column=0,padx=8,columnspan=2,sticky="ew")
         CTkToolTip(self.addon_repo, "格式: 作者/存储库名称")
@@ -70,21 +76,21 @@ class CreateAddons(ctk.CTkToplevel):
                 file=open(self.addon_dir.get()+"/"+self.addon_id.get()+"/info.yml","w")
                 file.write(yaml.safe_dump(data))
                 file.close()
-                info_window.info("info","创建成功")
+                info_window(self,"info","创建成功")
                 self.destroy()
             except:
-                info_window.info("bushi","你TM往输入框里填了什么奇奇怪怪的东西")
+                info_window(self,"bushi","你TM往输入框里填了什么奇奇怪怪的东西")
         else:
-            info_window.info("info","必要参数未填写")
+            info_window(self,"info","必要参数未填写")
 
 class Mian(ctk.CTkToplevel):
     def __init__(self,master):
         super().__init__(master)
         self.title("SimplerRSC")
         self.resizable(0,0)
-        self.geometry("250x150")
+        self.wm_attributes("-topmost", True)
         # 创建
-        ctk.CTkButton(self,text="创建新附属",command=lambda:CreateAddons(master),width=200,anchor="center").grid(row=0,column=0,padx=30,pady=17)
+        ctk.CTkButton(self,text="创建新附属",command=lambda:CreateAddons(master,self),width=200,anchor="center").grid(row=0,column=0,padx=30,pady=17)
         # 分割
         ctk.CTkLabel(self,text="或").grid(row=1,column=0)
         # 打开
